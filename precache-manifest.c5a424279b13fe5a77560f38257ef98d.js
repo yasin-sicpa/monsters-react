ec61775d711fbc04a1f48b8733e448a2600b5485
yasin-sicpa <Yasin.Koker@sicpa.com.tr>
2020-10-14 13:13:54 +0300

    Updates

---
+++ precache-manifest.c5a424279b13fe5a77560f38257ef98d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c109af147757b527de55957cf154c7",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "e1461362ab31110351ff",
+    "url": "/monsters-react/static/css/main.3a8efac2.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-react/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1461362ab31110351ff",
+    "url": "/monsters-react/static/js/main.1506df1d.chunk.js"
+  },
+  {
+    "revision": "135c228377aeb18d7c64",
+    "url": "/monsters-react/static/js/runtime-main.c275f0be.js"
+  }
+]);
